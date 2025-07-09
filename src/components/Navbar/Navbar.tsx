47aa1076dffe56bbbd0a6aca6298c60f4ed657e3
Irakli Tavartkiladze <IrakliTavartkiladze@Iraklis-MacBook-Pro.local>
2025-07-09 16:03:09 +0400

    first commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hourglass from './path/to/Hourglass.png';
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='navbar-body'>
+    <div>
+        <h1>Momentum     </h1>
+        
+    </div>
+    <div>
+    <button >თანამშრომლის შექმნა</button>
+     <button>+ შექმენი ახალი დავალება</button>
+    </div>
+    </div>
+     
+    
+  )
+}
+
+export default Navbar
